3be95cb6a3fa93b7997a9bee375955a4ce1afe44
pranikc <pranikc@gmail.com>
2016-10-07 20:08:33 -0500

    Creating classes in Javascript

--- classes.js
+++ classes.js
@@ -2,3 +2,10 @@
  * Created by pranikchainani on 10/7/16.
  */
 
+var car = function(){
+    console.log('Hey this is a function');
+};
+
+var Car = function () {
+    console.log('Hey this is a Class');
+};
